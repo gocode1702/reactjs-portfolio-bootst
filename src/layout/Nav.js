bae8537749f4c221dd7a086ea6859d3c790ee3f3
John Doe <cformation75@gmail.com>
2022-11-10 12:02:48 +0100

    oo

--- src/layout/Nav.js
+++ src/layout/Nav.js
@@ -7,7 +7,7 @@ function Thenavbar() {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="Projets">Projets</Nav.Link>
+            <Nav.Link href="Projets">Pro</Nav.Link>
             <Nav.Link href="WhoIAm">Portrait</Nav.Link>
             <Nav.Link href="Contact">Contact</Nav.Link>
           </Nav>
